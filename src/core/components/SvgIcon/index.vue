76e9efad464eff1661aa8b6c7dd0f8dfbc1f82ca
ShiHongyu <2071114140@qq.com>
2023-01-09 22:11:19 +0800

    可以定义单个icon的size

--- src/core/components/SvgIcon/index.vue
+++ src/core/components/SvgIcon/index.vue
@@ -1,31 +1,50 @@
 <template>
-  <svg class="icon" :class="[color ? 'customStyle' : false]" aria-hidden="true">
-    <use :xlink:href="`#${name}`" />
-  </svg>
+  <i class="icon-container">
+    <svg class="icon-svg" aria-hidden="true">
+      <use :xlink:href="`#${name}`" />
+    </svg>
+  </i>
 </template>
 
 <script setup lang="ts">
-
-defineProps<{
+import { computed } from 'vue'
+const props = withDefaults(defineProps<{
+  /**
+   * 通过指向svg syboml的id进行引用
+   */
   name: string;
-  color?: string;
-}>()
 
+  /**
+   * 指定大小
+   * 默认值：
+   *  size = 1
+   *  sizeUnit = 'em'
+   * 即，默认大小为1em
+   */
+  size?: number;
+  sizeUnit?: string;
+}>(), {
+  size: 1,
+  sizeUnit: 'em',
+})
+
+const sizeWithUnit = computed(() => `${props.size}${props.sizeUnit}`)
 
 </script>
 
 <style scoped>
-.customStyle {
-  color: v-bind(color);
+.icon-container {
+  overflow: hidden;
+  display: inline-block;
+  width: v-bind(sizeWithUnit);
+  height: v-bind(sizeWithUnit);
 }
-
 </style>
 
 <style>
-.icon {
-  display: inline-block;
-  width: 1em;
-  height: 1em;
-  fill: currentColor;
+.icon-svg {
+  display: block;
+  width: 100%;
+  height: 100%;
 }
 </style>
